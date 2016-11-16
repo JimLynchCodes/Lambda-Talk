83cbbb7e9b0d770bcf8e3ecb8feeee5443a96a07
Jim Lynch <mrdotjim@gmail.com>
2016-11-16 11:56:31 -0500

    Update lambda-function.js

--- code-examples/echo-as-array/lambda-function.js
+++ code-examples/echo-as-array/lambda-function.js
@@ -3,8 +3,8 @@ exports.handler = (event, context, callback) => {
   console.log('first param was: ' + event[0]);
   console.log('second param was: ' + event[1]);
   
-  let firstIncremented = event[0];
-  let secondIncremented = event[1];
+  let firstIncremented = event[0] + 1;
+  let secondIncremented = event[1] + 1;
 
   callback(null, [firstIncremented, secondIncremented]);
 };
